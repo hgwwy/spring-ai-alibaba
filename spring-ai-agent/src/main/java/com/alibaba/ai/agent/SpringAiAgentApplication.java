edb00574ccce149ad379895feefd0f7874b2b0af
wwy <544832037@qq.com>
2026-06-04 10:58:56 +0800

    第一次提交

---
+++ spring-ai-agent/src/main/java/com/alibaba/ai/agent/SpringAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.ai.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiAgentApplication.class, args);
+    }
+
+}
---
+++ spring-ai-agent/src/test/java/com/alibaba/ai/agent/SpringAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.ai.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
